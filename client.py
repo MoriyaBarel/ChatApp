ea008098b632a990a04fe7aa50382e3a29b9eb0e
MoriyaBarel <93195727+MoriyaBarel@users.noreply.github.com>
2022-09-10 01:52:33 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import math
+import os
 import sys
 import threading
 import time
@@ -13,22 +14,16 @@ def receive():
     """ Handles receiving of messages. """
     while True:
         try:
-            msg = sock.recv(BUF_SIZE).decode("utf8")
-            msg_list.insert(tkinter.END, msg)
+            msg = tcp_sock.recv(BUF_SIZE).decode("utf8")
+            MSG_LIST.insert(tkinter.END, msg)
             if msg == "quit":
-                sock.close()
-                top.quit()
+                tcp_sock.close()
+                TOP.quit()
             elif msg.startswith('##download'):
-                # Todo: decide between the following two options:
-                # option 1:
-                msg_data = msg.split("##download#")
-                details = msg_data[1]
-                # option 2:
-                # download_prefix = 11
-                # details = msg[download_prefix:]
-                save_as, file_type, file_size, recv_port, management_port = details.split('#')
+                details = msg[DOWNLOAD_PREFIX:]
+                save_as, file_type, file_size, recv_port, mgmt_port = details.split('#')
                 all_data = {}
-                download_thread = threading.Thread(target=download_file(all_data, save_as, file_type, file_size, recv_port, management_port))
+                download_thread = threading.Thread(target=download_file(all_data, save_as, file_type, file_size, recv_port, mgmt_port))
                 download_thread.start()
         except OSError:  # Possibly client has left the chat.
             break
@@ -36,42 +31,42 @@ def receive():
 
 def send(event=None):
     """ Handles sending of messages. """
-    msg = my_msg.get()
-    my_msg.set("")  # Clears input field.
-    sock.send(bytes(msg, "utf8"))
+    msg = MY_MSG.get()
+    MY_MSG.set("")  # Clears input field.
+    tcp_sock.send(bytes(msg, "utf8"))
     if msg == "#quit":
-        sock.close()
-        top.quit()
+        tcp_sock.close()
+        TOP.quit()
 
 
 def request(event=None):
-    my_msg.set("!request")
+    MY_MSG.set("!request")
     send()
 
 
 def closing(event=None):
     """ This function is to be called when the window is closed. """
-    my_msg.set("#quit")
+    MY_MSG.set("#quit")
     send()
 
 
 def get_users_button(event=None):
-    my_msg.set("#getusers")
+    MY_MSG.set("#getusers")
     send()
 
 
 def get_file_list(event=None):
-    my_msg.set("#getfilelist")
+    MY_MSG.set("#getfilelist")
     send()
 
 
-def download_file(all_data, save_as, file_type, file_size, recv_port, management_port):
-    udp_sock_management = socket(AF_INET, SOCK_DGRAM)
+def download_file(all_data, save_as, file_type, file_size, recv_port, mgmt_port):
+    udp_sock_mgmt = socket(AF_INET, SOCK_DGRAM)
     udp_sock_receive = socket(AF_INET, SOCK_DGRAM)
-    address = sock.getsockname()[0]
+    address = tcp_sock.getsockname()[0]
     udp_sock_receive.bind((address, int(recv_port)))
     packets_num = math.ceil(float(file_size)/CHUNK_SIZE)
-    mgmt_thread = threading.Thread(target=mgmt_loop, args=(all_data, packets_num, udp_sock_management, management_port))
+    mgmt_thread = threading.Thread(target=mgmt_loop, args=(all_data, packets_num, udp_sock_mgmt, mgmt_port))
     mgmt_thread.start()
     while True:
         packet_counter = len(all_data)
@@ -82,101 +77,108 @@ def download_file(all_data, save_as, file_type, file_size, recv_port, management
             if not bytes_read:
                 continue
             bytes_to_write, seq = bytes_read.split('seq:'.encode())
-            all_data[int(seq.decode("utf-8"))] = bytes_to_write
-    mgmt_thread.join()
+            all_data[int(seq.decode("utf8"))] = bytes_to_write
     write_to_file(all_data, save_as, file_type)
-    udp_sock_management.close()
+    mgmt_thread.join()
+    udp_sock_mgmt.close()
     udp_sock_receive.close()
-    return
+    MSG_LIST.insert(tkinter.END, "File download completed successfully")
 
 
-def mgmt_loop(all_data, packets_num, udp_sock_management, management_port):
+def mgmt_loop(all_data, packets_num, udp_sock_mgmt, mgmt_port):
     while True:
         packet_counter = len(all_data)
         if packet_counter == packets_num:
-            udp_sock_management.sendto('done'.encode(), (HOST, int(management_port)))
+            udp_sock_mgmt.sendto('done'.encode(), (HOST, int(mgmt_port)))
             print('download finished')
             break
-        handle_missing_packets(all_data, packets_num, udp_sock_management, management_port)
+        handle_missing_packets(all_data, packets_num, udp_sock_mgmt, mgmt_port)
         time.sleep(0.2)
 
 
-def handle_missing_packets(all_data, packets_num, udp_sock_management, management_port):
+def handle_missing_packets(all_data, packets_num, udp_sock_mgmt, mgmt_port):
     missing_packets = ''
     for i in range(0, packets_num):
         if i not in all_data.keys():
             packet_num = str(i)
             missing_packets = missing_packets + packet_num + ','
     missing_packets = missing_packets[:-1]
-    print('missing:', missing_packets)
-    udp_sock_management.sendto(missing_packets.encode(), (HOST, int(management_port)))
+    udp_sock_mgmt.sendto(missing_packets.encode(), (HOST, int(mgmt_port)))
 
 
 def write_to_file(all_data, save_as, file_type):
     name = save_as + '.' + file_type
-    file = open(name, 'wb')
+    file_path = os.path.join('download', name)
+    file = open(file_path, 'wb')
     for index in sorted(all_data.keys()):
         file.write(all_data[index])
     file.close()
 
 
 # ------------------------------Gui----------------------------------
-top = tkinter.Tk()
-top.title("ChatApp")
-messages_frame = tkinter.Frame(top)
+def init_gui():
+    top = tkinter.Tk()
+    top.title("ChatApp")
+    messages_frame = tkinter.Frame(top)
 
-my_msg = tkinter.StringVar()  # For the messages to be sent.
-my_msg.set("")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-msg_list = tkinter.Listbox(messages_frame, height=15, width=75, yscrollcommand=scrollbar.set)
-scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
-st = scrolledtext.ScrolledText(top)
+    my_msg = tkinter.StringVar()  # For the messages to be sent.
+    my_msg.set("")
+    scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+    msg_list = tkinter.Listbox(messages_frame, height=15, width=75, yscrollcommand=scrollbar.set)
+    scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+    msg_list.pack()
+    st = scrolledtext.ScrolledText(top)
 
-messages_frame.pack()
+    messages_frame.pack()
 
-lines = ["• To send a private message type @user_name and the message contents.",
-         "• To broadcast to everyone, just type the message.",
-         "• The download process begins with requesting permission",
-         "   by pressing the request button, than follow the instructions."]
-msg_list.insert(tkinter.END, 'User guide:')
-msg_list.insert(tkinter.END, lines[0])
-msg_list.insert(tkinter.END, lines[1])
-msg_list.insert(tkinter.END, lines[2])
-msg_list.insert(tkinter.END, lines[3])
+    lines = ["• To send a private message type @user_name and the message contents.",
+             "• To broadcast to everyone, just type the message.",
+             "• The download process begins with requesting permission",
+             "   by pressing the request button, than follow the instructions."]
+    msg_list.insert(tkinter.END, 'User guide:')
+    msg_list.insert(tkinter.END, lines[0])
+    msg_list.insert(tkinter.END, lines[1])
+    msg_list.insert(tkinter.END, lines[2])
+    msg_list.insert(tkinter.END, lines[3])
 
-button_label = tkinter.Label(top, text="Enter Message:")
-button_label.pack()
-entry_field = tkinter.Entry(top, textvariable=my_msg, foreground="Red")
-entry_field.bind("<Return>", send)
-entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
-send_button.pack()
-users_button = tkinter.Button(top, text="Online users", command=get_users_button)
-users_button.pack()
-request_button = tkinter.Button(top, text="Request download", command=request)
-request_button.pack()
-file_list_button = tkinter.Button(top, text="File list", command=get_file_list)
-file_list_button.pack()
+    button_label = tkinter.Label(top, text="Enter Message:")
+    button_label.pack()
+    entry_field = tkinter.Entry(top, textvariable=my_msg, foreground="Red")
+    entry_field.bind("<Return>", send)
+    entry_field.pack()
+    send_button = tkinter.Button(top, text="Send", command=send)
+    send_button.pack()
+    users_button = tkinter.Button(top, text="Online users", command=get_users_button)
+    users_button.pack()
+    request_button = tkinter.Button(top, text="Request download", command=request)
+    request_button.pack()
+    file_list_button = tkinter.Button(top, text="File list", command=get_file_list)
+    file_list_button.pack()
 
-quit_button = tkinter.Button(top, text="Quit", command=closing)
-quit_button.pack()
+    quit_button = tkinter.Button(top, text="Quit", command=closing)
+    quit_button.pack()
 
-top.protocol("WM_DELETE_WINDOW", closing)
+    top.protocol("WM_DELETE_WINDOW", closing)
+
+    return my_msg, msg_list, top
 
 # -------------------------------------------------------------------
 
+
 if __name__ == '__main__':
+    MY_MSG, MSG_LIST, TOP = init_gui()
     HOST = sys.argv[1]
     PORT = 55000
-    BUF_SIZE = 2048
     # BUF_SIZE is the total size of the packet
-    CHUNK_SIZE = 2000
+    BUF_SIZE = 2048
     # CHUNK SIZE is the data size, must be smaller than BUF_SIZE
+    CHUNK_SIZE = 2000
+    DOWNLOAD_PREFIX = 11
     ADDR = (HOST, PORT)
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.connect(ADDR)
+
+    tcp_sock = socket(AF_INET, SOCK_STREAM)
+    tcp_sock.connect(ADDR)
 
     receive_thread = Thread(target=receive)
     receive_thread.start()
--- server.py
+++ server.py
@@ -1,23 +1,19 @@
 import os
-import time
 from socket import *
 from threading import Thread
 
-all_ports = {55001: True, 55002: True, 55003: True, 55004: True, 55005: True, 55006: True, 55007: True, 55008: True,
-             55009: True, 55010: True, 55011: True, 55012: True, 55013: True, 55014: True, 55015: True}
+
+all_ports = {n: True for n in range(55001, 55016)}
 clients = {}
 addresses = {}
-files = ["random", "messi", "text"]
+files = os.listdir('files')
 PORT = 55000
 BUF_SIZE = 2048
 CHUNK_SIZE = 2000
 SOCK = socket(AF_INET, SOCK_STREAM)
-SOCK.bind(('', PORT))
-SOCK.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 # Check if a request has been sent
 check = {}
-for i in range(len(clients.values())):
-    check[i] = False
+MAX_CLIENTS = 15
 
 
 def accept_connections():
@@ -25,58 +21,60 @@ def accept_connections():
     while True:
         client, client_address = SOCK.accept()
         print(str(client_address) + " has connected.")
-        client.send(f' Greetings!\nNow type your name and press enter !'.encode("utf8"))
+        client.send(b'Greetings!Now type your name and press enter.')
         addresses[client] = client_address
         Thread(target=handle_client, args=(client, client_address)).start()
 
 
-def handle_client(conn, addr):  # Takes client socket as argument.
+def handle_client(tcp_sock, addr):  # Takes client socket as argument.
     """Handles a single client connection."""
-    name = conn.recv(BUF_SIZE).decode("utf8")
+    name = tcp_sock.recv(BUF_SIZE).decode("utf8")
     for nick in clients.values():
         if nick == name:
-            name = name + str(1)
-    welcome = 'Welcome %s! If you ever want to quit, press quit button to exit.' % name
-    conn.send(bytes(welcome, "utf8"))
-    msg = "%s from [%s] has joined the chat!" % (name, "{}:{}".format(addr[0], addr[1]))
+            name = name + "1"
+    welcome = f'Welcome {name}! If you ever want to quit, press quit button to exit.'
+    tcp_sock.send(bytes(welcome, "utf8"))
+    msg = f'{name} from [{addr[0], addr[1]}] has joined the chat!'
     broadcast(bytes(msg, "utf8"))
-    clients[conn] = name
+    clients[name] = tcp_sock
+    check[name] = False
     while True:
-        msg = conn.recv(BUF_SIZE)
-        if msg == bytes("!request", "utf8"):
-            request_file(conn, msg)
-        elif msg.startswith("##".encode()):
-            Thread(target=send_file(conn, msg, addr[0]))
-        elif msg == bytes("#getusers", "utf8"):
-            get_users(conn, msg, "connected users: ")
-        elif msg == bytes("#getfilelist", "utf8"):
-            get_file_list(conn, msg, " file list->")
-        elif msg.startswith('@'.encode()):
-            private_message(conn, msg, name + ":")
-        elif msg != bytes("#quit", "utf8"):
+        msg = tcp_sock.recv(BUF_SIZE)
+        if msg == b'!request':
+            request_file(tcp_sock, name, msg)
+        elif msg.startswith(b'##'):
+            Thread(target=pre_sending(tcp_sock, name, msg, addr[0]))
+        elif msg == b'#getusers':
+            get_users(tcp_sock, msg, "connected users: ")
+        elif msg == b'#getfilelist':
+            get_file_list(tcp_sock, msg, "file list->")
+        elif msg.startswith(b'@'):
+            private_message(tcp_sock, msg, name + ":")
+        elif msg != b'#quit':
             broadcast(msg, name + ":")
         else:
-            del clients[conn]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
-            conn.close()
+            del clients[name]
+            broadcast(bytes(f'{name} has left the chat.', "utf8"))
+            tcp_sock.close()
             break
 
 
-def send_file(conn, msg, client_address):
-    if check[clients[conn]]:
+def pre_sending(tcp_sock, name, msg, client_address):
+    if check[name]:
         file_name_and_type, file_name, file_type, save_as = get_filename_and_filetype(msg)
-        if file_name not in files:
-            conn.send('error: file not found'.encode())
+        if file_name_and_type not in files:
+            tcp_sock.send(b'error: file not found')
         else:
-            path = os.path.abspath(file_name_and_type)
-            file_size = os.path.getsize(file_name_and_type)
-            send_port = get_port(conn)
-            management_port = get_port(conn)
-            all_data = prep_data_for_sending(path)
-            conn.send(f'##download{"#"}{save_as}{"#"}{file_type}{"#"}{file_size}{"#"}{send_port}{"#"}{management_port}'.encode())
-            sending(conn, client_address, send_port, management_port, all_data)
+            file_path = os.path.join('files', file_name_and_type)
+            abs_path = os.path.abspath(file_path)
+            file_size = os.path.getsize(file_path)
+            send_port = get_port(tcp_sock)
+            mgmt_port = get_port(tcp_sock)
+            all_data = prep_data_for_sending(abs_path)
+            tcp_sock.send(f'##download{"#"}{save_as}{"#"}{file_type}{"#"}{file_size}{"#"}{send_port}{"#"}{mgmt_port}'.encode())
+            send_file(name, client_address, send_port, mgmt_port, all_data)
     else:
-        conn.send(bytes("request before download", "utf8"))
+        tcp_sock.send(b'request before download')
 
 
 def prep_data_for_sending(path):
@@ -88,104 +86,95 @@ def prep_data_for_sending(path):
         if not bytes_read:
             break
         seq_num = str(seq).encode()
-        bytes_to_send = bytes_read + 'seq:'.encode() + seq_num
+        bytes_to_send = bytes_read + b'seq:' + seq_num
         all_data[seq] = bytes_to_send
         seq += 1
     file.close()
     return all_data
 
 
-def sending(conn, client_address, send_port, management_port, all_data):
+def send_file(name, client_address, send_port, mgmt_port, all_data):
     udp_sock_send = socket(AF_INET, SOCK_DGRAM)
-    udp_sock_management = socket(AF_INET, SOCK_DGRAM)
-    udp_sock_management.bind((client_address, management_port))
+    udp_sock_mgmt = socket(AF_INET, SOCK_DGRAM)
+    udp_sock_mgmt.bind((client_address, mgmt_port))
     while True:
         # nack means negative acknowledge
-        nack = udp_sock_management.recv(BUF_SIZE).decode("utf-8")
-        print("nack", nack)
+        nack = udp_sock_mgmt.recv(BUF_SIZE).decode("utf-8")
         if 'done' in nack:
-            conn.send('File download completed successfully'.encode())
             all_ports[send_port] = True
-            all_ports[management_port] = True
-            check[clients[conn]] = False
+            all_ports[mgmt_port] = True
+            check[name] = False
             print('done')
             break
         else:
             packets_to_send = nack.split(',')
             for packet_num in set(packets_to_send):
                 udp_sock_send.sendto(all_data[int(packet_num)], (client_address, send_port))
-    udp_sock_management.close()
+    udp_sock_mgmt.close()
     udp_sock_send.close()
 
 
 def get_filename_and_filetype(msg: bytes):
-    actual_msg = msg[2:]
-    msg_str = actual_msg.decode("utf-8").split("#")
-    file_name_and_type = msg_str[0]
-    save_as = msg_str[1]
-    file = file_name_and_type.split('.')
-    filename = file[0]
-    file_type = file[1]
+    MSG_PREFIX = 2
+    actual_msg = msg[MSG_PREFIX:]
+    file_name_and_type, save_as = actual_msg.decode("utf-8").split("#")
+    filename, file_type = file_name_and_type.split('.')
     return file_name_and_type, filename, file_type, save_as
 
 
-def request_file(conn, msg):
-    if msg == '!request'.encode():
-        message = "To continue the download, enter the file name with ##file_name.file_type#save_as\n"
-        conn.send(message.encode())
-        time.sleep(1)
-        check[clients[conn]] = True
+def request_file(tcp_sock, name, msg):
+    if msg == b'!request':
+        message = "To continue the download, enter the file name with ##file_name.file_type#save_as"
+        tcp_sock.send(message.encode())
+        check[name] = True
 
 
-def get_users(conn, msg, prefix=""):
-    if msg == '#getusers'.encode():
-        connected = str(clients.values())
-        connected_users = connected[11:]
-        conn.send(bytes(prefix, "utf8") + connected_users.encode())
+def get_users(tcp_sock, msg, prefix=""):
+    if msg == b'#getusers':
+        connected = str(clients.keys())
+        MSG_PREFIX = 10
+        connected_users = connected[MSG_PREFIX:-1]
+        tcp_sock.send(bytes(prefix, "utf8") + connected_users.encode())
 
 
-def get_file_list(conn, msg, prefix=""):
-    if msg == '#getfilelist'.encode():
+def get_file_list(tcp_sock, msg, prefix=""):
+    if msg == b'#getfilelist':
         file_list_str = str(files)
-        conn.send(bytes(prefix, "utf8") + file_list_str.encode())
+        tcp_sock.send(bytes(prefix, "utf8") + file_list_str.encode())
 
 
-def private_message(conn, msg, prefix=""):
+def private_message(tcp_sock, msg, prefix=""):
     check_name = False
-    for name in clients.values():
-        sock = get_key(name)
+    for name in clients:
+        sock = clients[name]
         name_len = len(name)
         if msg[1:name_len + 1].decode() == name:
             check_name = True
             actual_message = msg[name_len + 1:]
             sock.send(bytes(prefix, "utf8") + actual_message)
-            conn.send(bytes(prefix, "utf8") + actual_message)
+            tcp_sock.send(bytes(prefix, "utf8") + actual_message)
             break
     if not check_name:
-        conn.send(f"user not found".encode())
+        tcp_sock.send(b'user not found')
 
 
 def broadcast(msg, prefix=""):
-    for sock in clients:
+    for sock in clients.values():
         sock.send(bytes(prefix, "utf8") + msg)
 
 
-def get_port(conn):
+def get_port(tcp_sock):
     for key in all_ports:
         if all_ports[key]:
             all_ports[key] = False
             return key
-    conn.send('there is no available ports, please try again later:)'.encode())
-
-
-def get_key(val):
-    for key, value in clients.items():
-        if val == value:
-            return key
+    tcp_sock.send(b'there is no available ports, please try again later:)')
 
 
 if __name__ == "__main__":
-    SOCK.listen(5)
+    SOCK.bind(('', PORT))
+    SOCK.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    SOCK.listen(MAX_CLIENTS)
     print("Chat Server has Started !!")
     print("Waiting for incoming connections...")
     ACCEPT_THREAD = Thread(target=accept_connections)
